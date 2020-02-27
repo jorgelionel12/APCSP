9c27481828b5996b474501ce4ddac5b02ef4035b
Desmond Sinnott <desmondsinnott@ORRHS.ORG>
2020-02-27 13:48:20 -0500

    ddd

---
+++ 54 String Codes/app.js
@@ -0,0 +1,37 @@
+let string = 'my name is Desi'
+
+console.log(string.length)
+
+let num = 13
+let s = num.toString()
+
+
+//SEARCH
+s = string.search('D')
+
+s = string.startsWith('F')
+
+s = string.includes('D')
+
+//Modifying String
+
+s = string.toLowerCase()
+
+s = string.toUpperCase()
+
+s = string.concat('I is Jesus')
+
+s = string.slice(3,7)
+
+
+s = string.trim()
+
+
+
+
+console.log(s)
+
+//CONVERT TO NUMBER
+string = 3
+num = Number(string)
+console.log(typeof num, num)
