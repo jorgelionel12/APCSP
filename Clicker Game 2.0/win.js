bffa791420d7841556382a641cea8798da5fc29c
jorgelionel12 <59832337+jorgelionel12@users.noreply.github.com>
2020-03-03 14:28:33 -0500

    Update win.js

--- Clicker Game 2.0/win.js
+++ Clicker Game 2.0/win.js
@@ -1,5 +1,16 @@
 function setup(){
-  
-  
-  
+  function setup(){
+  createCanvas(windowWidth, windowHeight)
+  heading = createElement('h2' 'You Won the game');
+  heading.position(width / 3, height / 4);
+  button = createButton('Go Back to Game);
+  button.position(width / 2, height / 2);
+  button.mousePressed(goToLink)
 }
+
+function goToLink(){
+  window.location.href = "game.html";
+}
+  
+  
+
